28fdc15c22d86b2f98e2658a633db01cbc6b135d
Gustavo Bento <gustavobentoteles379@gmail.com>
2025-06-08 12:22:44 -0300

    Add autocmds para arquivos .js e .ts

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,3 +17,23 @@ vim.api.nvim_create_autocmd("FileType",{
     vim.bo.expandtab = true
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript",
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescript",
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
